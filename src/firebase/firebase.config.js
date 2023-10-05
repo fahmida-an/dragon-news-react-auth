5b81cfb01947f4aab39a6f532e3da6be2214a86e
Fahmida Khan <fahmidakhann.19@gmail.com>
2023-10-05 13:59:12 +0600

    tailwind, daisyui and firebase setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbpdHDTFDHphXkgqJ5pV1g2lvd1s9fFJA",
+  authDomain: "dragon-news-react-auth-56aca.firebaseapp.com",
+  projectId: "dragon-news-react-auth-56aca",
+  storageBucket: "dragon-news-react-auth-56aca.appspot.com",
+  messagingSenderId: "805989080875",
+  appId: "1:805989080875:web:ef5595dc53334b98616f6e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
