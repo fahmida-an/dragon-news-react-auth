9d9e5176968e09d519944b82d795bcb13e998b41
Fahmida Khan <fahmidakhann.19@gmail.com>
2023-10-05 16:38:36 +0600

    Designed Header

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+
+import headerImage from '../../../assets/logo.png'
+import moment from 'moment';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is header</h2>
+        <div className='items-center space-y-3 mt-12 mb-8'>
+            <img className='mx-auto ' src={headerImage} alt="" />
+            <p className='text-center text-dark1 text-lg'>Journalism Without Fear or Favour</p>
+            <p className='text-dark2 text-center text-xl'>{moment().format('dddd, MMMM D, YYYY, h:mm:ss a')}</p>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+     dark1: '#706F6F',
+     dark2: '#403F3F',
+     dark3: '#9F9F9F',
+    },
+
     extend: {
       fontFamily: {
-        display: "'Poppins', sans-serif"
-      }
+        display: "'Poppins', sans-serif",
+      },
     },
   },
   plugins: [require("daisyui")],
-}
-
+};
