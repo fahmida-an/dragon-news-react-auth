c217a49398d069f900b514345e4fbedc7c1695e6
Fahmida Khan <fahmidakhann.19@gmail.com>
2023-10-05 19:19:54 +0600

    Added a navbar

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,11 +1,51 @@
-
+import { NavLink } from "react-router-dom";
+import userImage from '../../../assets/user.png';
 
 const Navbar = () => {
-    return (
+
+  const navLinks = (
+    <>
+      <li className="mr-4">
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li className="mr-4">
+        <NavLink to="/about">About</NavLink>
+      </li>
+      <li className="mr-4">
+        <NavLink to="/career">Career</NavLink>
+      </li>
+    </>
+  );
+  return (
+  <div>
+    <div className="navbar ">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+       {navLinks}
+      </ul>
+    </div>
+    
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      {navLinks}
+    </ul>
+  </div>
+  <div className="navbar-end">
+  <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
         <div>
-            <h2>This is navbar</h2>
+            <img className="w-10 rounded-full" src={userImage} alt="" />
         </div>
-    );
+      </label>
+    <a className="btn bg-dark1">Login</a>
+  </div>
+</div>
+  </div>
+  );
 };
 
 export default Navbar;
