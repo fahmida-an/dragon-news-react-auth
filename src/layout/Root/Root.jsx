3a74f1156464855519e616de6145bbdd17d5eefe
Fahmida Khan <fahmidakhann.19@gmail.com>
2023-10-05 15:41:07 +0600

    Router and font setup

---
+++ src/layout/Root/Root.jsx
@@ -0,0 +1,10 @@
+import { Outlet } from 'react-router-dom';
+const Root = () => {
+    return (
+        <div className='font-display'>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Root;
